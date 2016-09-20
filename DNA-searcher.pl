959c5d1f8cccc7626a4b9557e1effee48adb02f6
passdan <daniel.antony.pass@googlemail.com>
2016-09-20 17:18:56 +0100

    Add Mars

--- DNA-searcher.pl
+++ DNA-searcher.pl
@@ -1,24 +1,33 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+
+####################################################
+## Daniel Pass | github.com/passdan | August 2016 ##
+####################################################
+
+# Print to the screen slower for effect!
 $| = 1;
-#select(undef, undef, undef, 0.25);
 
 my %sequenceDB;
 
-if(@ARGV == 0){
-  print "Loading default database: Guess Who!\n";sleep(1);
+# Load Database
+print "Which Database to load:\n- GuessWho\n- Mars\n---\n";
+my $dbchoice = <STDIN>;
+chomp($dbchoice);
+exit 0 if ($dbchoice eq ""); # If empty string, exit.
+
+if(lc $dbchoice eq lc "GuessWho"){
+  print "Loading Guess Who database!\n";sleep(1);
   loadGuessWhoDB();
+}elsif(lc $dbchoice eq lc "Mars"){
+  print "Loading Mars database!\n";sleep(1);
+  loadMarsDB();
 }else{
-  open(DB, '<', $ARGV[0]) or die "Couldnt load the $ARGV[0] database";
-  while(<DB>){
-    chomp;
-    my @line = split('\t', $_);
-    $sequenceDB{$line[0]} = $line[1];
-  }
+print "I didn't understand that choice, exiting\n";
+exit 0
 }
 
-print "Sequence Database loaded...\n";sleep(1);
 print "Gene options are:\n";
 foreach my $key (sort { $sequenceDB{$a} cmp $sequenceDB{$b} } keys %sequenceDB){
   print "$key\t$sequenceDB{$key}\n";
@@ -26,7 +35,12 @@ foreach my $key (sort { $sequenceDB{$a} cmp $sequenceDB{$b} } keys %sequenceDB){
 }
 sleep(2);
 
-my $prompt = "\n#######################################################\n## Type a DNA sequence to search for what gene it is from ##\n#######################################################\n\nInput: ";
+my $prompt = "
+############################################################
+## Type a DNA sequence to search for what gene it is from ##
+############################################################
+
+Input: ";
 
 system("clear");
 print $prompt;
@@ -38,7 +52,13 @@ while (my $seq = <STDIN>){
     print "Searching: 0%"; sleep(1);
     while ($n < 100){ $n+=25; print "----$n%";sleep(1);};
     my $genestring = $sequenceDB{$seq};
-    print "\n\nThis DNA sequence is found in the gene for:  "; sleep(1);
+    # Guess Who return line
+    if (lc $dbchoice eq lc "GuessWho"){
+	    print "\n\nThis DNA sequence is found in the gene for:  "; sleep(1);
+    # Mars return line
+    }elsif(lc $dbchoice eq lc "Mars"){
+	    print "\n\nThis DNA sequence is most similar to : "; sleep(1);
+    }
     foreach (split //, $genestring){
       print;
       select(undef, undef, undef, 0.1);
@@ -81,8 +101,30 @@ sub loadGuessWhoDB {
   CCACTGGATCTA => "Ginger Hair",            #Hair Colour
   GTGATGCCGTGG => "Boy",                    #Sex
   AATACACCCGTG => "Girl",                   #Sex
-  GAGTAACGCATC => "Doesn't need glasses"    #Eyesight
+  GAGTAACGCATC => "Doesn't need glasses",    #Eyesight
   GTCTCCGATATG => "Needs glasses"           #Eyesight
+  );
+  return;
+}
+
+sub loadMarsDB {
+
+  %sequenceDB = (
+  TAGTGGAGCATC => "",      #Ears
+  CGGAATAACATG => "Unknown Species!",      #Ears
+  CCAGTGGATGGC => "Methanosarcina barkeri",             #Eyes
+  AGTCCAGTGGTA => "Yersinia pestis",              #Eyes
+  CAGCGGTTGCTA => "Ulva intestinalis",             #Eyes
+  GGGACATTCCTG => "",             #Hair Type
+  TTAGCCGCAACG => "Homo Sapien",          #Hair Type
+  TATGTTGAAGGC => "Bradyrhizobium japonicum",            #Hair Colour
+  AACTCCCGGATA => "Cyanobacteria",             #Hair Colour
+  TGATAGACTGGC => "Campylobacter jejuni",             #Hair Colour
+  CCACTGGATCTA => "Salmonela enterica",            #Hair Colour
+  GTGATGCCGTGG => "",                    #Sex
+  AATACACCCGTG => "Agaricus bisporus",                   #Sex
+  GAGTAACGCATC => "Influenza",    #Eyesight
+  ATCGTTGGCGAC => "Homo sapien"           #Eyesight
   );
   return;
 }
